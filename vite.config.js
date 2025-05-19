fa3216e9b79da0e795ab3d5a518309a612c0b1e3
Guhan Santhanam <spguhansanthanam@gmail.com>
2025-05-20 00:54:24 +0530

    Added Title and Favicon

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio-guhansp/",
+  base: "/guhansp-portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
